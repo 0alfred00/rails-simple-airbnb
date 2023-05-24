fc8fb11740e4b937d41679124a016039cf0a841b
Alfred Seeliger <seeliger.alfred@gmail.com>
2023-05-24 16:39:03 +0200

    basic setup done, routes, model, migration, seeding, view, controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times do
+  Flat.create!(
+    name: Faker::Address.community,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph_by_chars(number: 256),
+    price_per_night: rand(50..200),
+    number_of_guests: rand(1..6)
+  )
+end
